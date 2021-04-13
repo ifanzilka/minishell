9497ba6061e48696ae5f6524d0238729809acd8c
Blood Marillion <bmarilli@ev-m6.kzn.21-school.ru>
2021-04-13 17:37:45 +0300

    add emodi

--- srcs/minishell/ft_print_shell.c
+++ srcs/minishell/ft_print_shell.c
@@ -14,7 +14,18 @@
 
 void	ft_print_shell(void)
 {
-	ft_putstr_fd("\033[32m", 1);
-	ft_putstr_fd("minishell$> ", 1);
-	ft_putstr_fd("\033[37m", 1);
+	ft_putstr_fd("minishell", 1);
+	if (g_exit_status == 0)
+	{
+		ft_putstr_fd("\033[32m", 1);
+		ft_putstr_fd("(o-_-o)", 1);
+		ft_putstr_fd("\033[37m", 1);
+	}
+	else
+	{
+		ft_putstr_fd("\033[31m", 1);
+		ft_putstr_fd("(＞-＜)", 1);
+		ft_putstr_fd("\033[37m", 1);
+	}
+	ft_putstr_fd("$> ", 1);
 }
