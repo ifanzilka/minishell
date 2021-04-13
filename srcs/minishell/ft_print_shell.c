75475ae0395523ccafcc59de5b5fe44c05b6950f
Blood Marillion <bmarilli@ev-m6.kzn.21-school.ru>
2021-04-13 17:44:08 +0300

    add color

--- srcs/minishell/ft_print_shell.c
+++ srcs/minishell/ft_print_shell.c
@@ -14,7 +14,9 @@
 
 void	ft_print_shell(void)
 {
+	ft_putstr_fd("\033[36m", 1);
 	ft_putstr_fd("minishell", 1);
+	ft_putstr_fd("\033[37m", 1);
 	if (g_exit_status == 0)
 	{
 		ft_putstr_fd("\033[32m", 1);
@@ -27,5 +29,7 @@ void	ft_print_shell(void)
 		ft_putstr_fd("(＞-＜)", 1);
 		ft_putstr_fd("\033[37m", 1);
 	}
+	ft_putstr_fd("\033[36m", 1);
 	ft_putstr_fd("$> ", 1);
+	ft_putstr_fd("\033[37m", 1);
 }
